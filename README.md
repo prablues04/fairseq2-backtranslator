b9ba781c14f2b5a8e233259d603f0b14ba77e6e0
Vedagiri, Prabhas <prabhas.vedagiri@outlook.com>
2024-11-18 18:49:43 +0000

    Update fine-tune.py to successfully translate between languages using SONAR

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Hello World
+# Indic NLP Library
+[Indic NLP Library](https://github.com/anoopkunchukuttan/indic_nlp_library/tree/master) was used for tokenising indian languages within NLLB-200 and also generally used for script conversion, romanization, indicisation, etc.
--- fine-tune.py
+++ fine-tune.py
@@ -1,26 +1,39 @@
 from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline
 from sonar.inference_pipelines.text import EmbeddingToTextModelPipeline
 from sonar.inference_pipelines.text import TextToTextModelPipeline
-
 import pandas as pd
+import time
 
 # Load the data
 class Data:
-    def __init__(self, path):
-        self.df = pd.read_csv(path)
+    def __init__(self, path=None, df=None):
+        self.df = pd.read_csv(path) if path else df
+        if not path and df is None:
+            print("Error: Either 'path' or 'df' must be provided and not None.")
+        elif path and df is not None:
+            print("Warning: Only one of 'path' or 'df' must be provided. Defaults to using 'path' as dataset.")
     
     def head(self):
         return self.df.head()
 
     def get_data(self):
         return self.df
+    
+    def get_data_as_list(self):
+        return self.df.values.flatten().tolist()
 
 class Text2TextModel:
     def __init__(self, encoder, decoder, tokenizer, max_seq_len=512):
         self.encoder = encoder
+        self.decoder = decoder
         self.tokenizer = tokenizer
         self.max_seq_len = max_seq_len
+        print("Reached here")
+        start_time = time.time()        
         self.t2t_model = TextToTextModelPipeline(encoder=self.encoder, decoder=self.decoder, tokenizer=self.tokenizer)
+        end_time = time.time()
+        print(f"Constructing t2t model took {end_time - start_time:.4f} seconds to complete.")
+        print("Reached here too")
 
     def create_embeddings(self, data, source_lang):
         """
@@ -29,7 +42,7 @@ class Text2TextModel:
         @param source_lang: str
         """
         t2vec_model = TextToEmbeddingModelPipeline(encoder=self.encoder, tokenizer=self.tokenizer)
-        sentences = data.df['text']
+        sentences = data.get_data_as_list()
         embeddings = t2vec_model.predict(sentences, source_lang=source_lang)
         return embeddings
 
@@ -50,13 +63,14 @@ class Text2TextModel:
         @param source_lang: str
         @param target_lang: str
         """
-        embeddings = self.create_embeddings(data, source_lang)
-        reconstructed = self.reconstruct_text(embeddings, target_lang)
-        return reconstructed
+        return self.t2t_model.predict(data.get_data_as_list(), source_lang=source_lang, target_lang=target_lang)
 
 
 if __name__ == "__main__":
+    data = Data(df=pd.DataFrame({'sentences':["Hello, How are you?", "India is a diverse country with many languages."]}))
+    print(data.head())
+    print(data.get_data())
+    print(data.get_data_as_list())
     model = Text2TextModel(encoder='text_sonar_basic_encoder', decoder='text_sonar_basic_decoder', tokenizer='text_sonar_basic_encoder')
-    data = Data("sentences-1M.csv")
-    translations = model.predict(data.get_data[:100], source_lang="tel_Telu", target_lang="eng_Latn")
-    print(translations.head())
+    translations = model.predict(data, source_lang="eng_Latn", target_lang="tel_Telu")
+    print(translations)
